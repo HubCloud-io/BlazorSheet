292ee3e7aef2c31634bb4b4df57bf3609149c093
Michael Zyubin <MikhaZyu@gmail.com>
2023-06-01 12:36:08 +0300

    Add unit tests for SheetData

---
+++ test/HubCloud.BlazorSheet.UnitTests/SheetDataTests.cs
@@ -0,0 +1,62 @@
+﻿using HubCloud.BlazorSheet.Core.Models;
+using NUnit.Framework;
+using System;
+
+namespace HubCloud.BlazorSheet.UnitTests;
+
+
+[TestFixture]
+public class SheetDataTests
+{
+    private SheetData _sheetData;
+
+    [SetUp]
+    public void Setup()
+    {
+        _sheetData = new SheetData(10, 10);
+    }
+
+    [Test]
+    public void IndexerGet_ValidIndices_ReturnsCorrectValue()
+    {
+        _sheetData[5, 5] = "TestValue";
+        Assert.AreEqual("TestValue", _sheetData[5, 5]);
+    }
+
+    [Test]
+    public void IndexerSet_ValidIndices_SetsCorrectValue()
+    {
+        _sheetData[5, 5] = "TestValue";
+        Assert.AreEqual("TestValue", _sheetData[5, 5]);
+    }
+
+    [Test]
+    public void IndexerGet_RowIndexOutOfRange_ThrowsIndexOutOfRangeException()
+    {
+        Assert.Throws<IndexOutOfRangeException>(() =>
+        {
+            var test = _sheetData[11, 5];
+        });
+    }
+
+    [Test]
+    public void IndexerSet_RowIndexOutOfRange_ThrowsIndexOutOfRangeException()
+    {
+        Assert.Throws<IndexOutOfRangeException>(() => { _sheetData[11, 5] = "TestValue"; });
+    }
+
+    [Test]
+    public void IndexerGet_ColumnIndexOutOfRange_ThrowsIndexOutOfRangeException()
+    {
+        Assert.Throws<IndexOutOfRangeException>(() =>
+        {
+            var test = _sheetData[5, 11];
+        });
+    }
+
+    [Test]
+    public void IndexerSet_ColumnIndexOutOfRange_ThrowsIndexOutOfRangeException()
+    {
+        Assert.Throws<IndexOutOfRangeException>(() => { _sheetData[5, 11] = "TestValue"; });
+    }
+}
