17cf184201999f12ec3705f15585dbc3dc3e9fe3
Oceanshiver <62504973+Oceanshiver@users.noreply.github.com>
2023-06-26 13:12:10 +0300

    misc

--- src/HubCloud.BlazorSheet.EvalEngine/Engine/FormulaConverter.cs
+++ src/HubCloud.BlazorSheet.EvalEngine/Engine/FormulaConverter.cs
@@ -1,12 +1,16 @@
-﻿namespace HubCloud.BlazorSheet.EvalEngine.Engine
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace HubCloud.BlazorSheet.EvalEngine.Engine
 {
     public class FormulaConverter
     {
-        public static string PrepareFormula(string formulaIn)
+        public static string PrepareFormula(string formulaIn, string contextName = "_cells")
         {
             var formula = formulaIn;
-
-           
             formula = formula
                 .Replace("=","==")
                 .Replace("====","==")
@@ -19,8 +23,106 @@
                 .Replace(" AND "," && ")
                 .Replace(" or "," || ")
                 .Replace(" OR "," || ");
-            
+
             return formula;
+            
+            // var funcDict = new Dictionary<string, string>();
+            // var sb = new StringBuilder(formula.Replace("$c", contextName));
+            //
+            // CutFunctions(sb, funcDict, contextName);
+            //
+            // sb = SetValues(sb);
+            //
+            // PasteFunctions(sb, funcDict);
+            //
+            // return sb.ToString();
+        }
+        
+        private static void PasteFunctions(StringBuilder sb, Dictionary<string, string> funcDict)
+        {
+            foreach (var item in funcDict.Reverse())
+            {
+                sb = sb.Replace(item.Key, item.Value);
+            }
+        }
+
+        private static void CutFunctions(StringBuilder sb, Dictionary<string, string> funcDict, string contextName)
+        {
+            var regex = new Regex(@"[A-z]+[(][^()]+[)]");
+            var functions = regex.Matches(sb.ToString())
+                .Cast<Match>()
+                .Select(m => m.Value)
+                .Distinct()
+                .ToList();
+
+            var currentIndent = 0;
+            foreach (var function in functions)
+            {
+                var processed = ProcessFunction(function, contextName);
+                var key = "{{" + currentIndent++ + "}}";
+                sb = sb.Replace(processed, key);
+                funcDict.Add(key, processed);
+            }
+        }
+
+        private static string ProcessFunction(string function, string contextName)
+        {
+            var startIndex = function.IndexOf("(", StringComparison.InvariantCulture);
+            var endIndex = function.IndexOf(")", StringComparison.InvariantCulture);
+
+            var parameters = function.Substring(startIndex + 1, endIndex - startIndex - 1)
+                .Split(',')
+                .ToList();
+
+            var values = new List<string>();
+            foreach (var parameter in parameters)
+            {
+                if (!parameter.Contains(':'))
+                    values.Add(PrepareFormula(parameter, contextName));
+                else
+                    values.Add(parameter);
+            }
+
+            var restoredFunction = function.Substring(0, startIndex + 1);
+            foreach (var value in values)
+            {
+                restoredFunction += $"{value},";
+            }
+
+            restoredFunction = restoredFunction.TrimEnd(',');
+            restoredFunction += function.Substring(endIndex, function.Length - endIndex);
+
+            return restoredFunction;
+        }
+
+        private static StringBuilder SetValues(StringBuilder sb)
+        {
+            var valueDict = GetValueDict(sb);
+            foreach (var item in valueDict)
+            {
+                sb = sb.Replace(item.Key, item.Value);
+            }
+
+            return sb;
+        }
+
+        private static Dictionary<string, string> GetValueDict(StringBuilder formula)
+            => GetValueDict(formula.ToString());
+
+        private static Dictionary<string, string> GetValueDict(string formula)
+        {
+            var regex = new Regex(@"R-*\d*C-*\d*");
+            var matches = regex.Matches(formula);
+
+            var dict = new Dictionary<string, string>();
+            foreach (var match in matches)
+            {
+                var key = match.ToString();
+                var val = $"_cells.GetValue(\"{key}\")";
+                dict.Add(key, val);
+            }
+
+            return dict;
         }
     }
 }
---
+++ test/HubCloud.BlazorSheet.UnitTests/FormulaConverterTests.cs
@@ -0,0 +1,23 @@
+﻿using HubCloud.BlazorSheet.EvalEngine.Engine;
+using NUnit.Framework;
+
+namespace HubCloud.BlazorSheet.UnitTests;
+
+[TestFixture]
+public class FormulaConverterTests
+{
+    private const string ContextName = "_cells";
+    
+    [TestCase("123", "123")]
+    [TestCase("R8C10+R-1C10", $@"{ContextName}.GetValue(""R8C10"")+{ContextName}.GetValue(""R-1C10"")")]
+    [TestCase(@"SUM(""R8C10:R-1C10"")", @"SUM(""R8C10:R-1C10"")")]
+    [TestCase(@"SUM(""R8C10:R-1C10"")+R8C10+R-1C10+SUM(""R8C10:R-1C10"")+SUM(""R8C11:R-1C10"")", $@"SUM(""R8C10:R-1C10"")+{ContextName}.GetValue(""R8C10"")+{ContextName}.GetValue(""R-1C10"")+SUM(""R8C10:R-1C10"")+SUM(""R8C11:R-1C10"")")]
+    [TestCase("IIF(R1C2 = 0, R4C5, 2)", $@"IIF({ContextName}.GetValue(""R1C2"") = 0, {ContextName}.GetValue(""R4C5""), 2)")]
+    [TestCase("IIF(R1C1 > 10, IIF(R1C2 = 0, 1, 2) + R4C5, 45)", $@"IIF({ContextName}.GetValue(""R1C1"") > 10, IIF({ContextName}.GetValue(""R1C2"") = 0, 1, 2) + {ContextName}.GetValue(""R4C5""), 45)")]
+    public void PrepareFormula_Tests(string formulaIn, string expected)
+    {
+        var formulaOut = FormulaConverter.PrepareFormula(formulaIn, ContextName);
+        
+        Assert.AreEqual(expected, formulaOut);
+    }
+}
