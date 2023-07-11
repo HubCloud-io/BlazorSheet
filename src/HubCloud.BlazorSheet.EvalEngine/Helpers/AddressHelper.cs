2084cb8b8632e69f65e050a97c1f1ac76bb60301
Oceanshiver <62504973+Oceanshiver@users.noreply.github.com>
2023-07-11 13:59:20 +0300

    misc

---
+++ src/HubCloud.BlazorSheet.EvalEngine/Helpers/AddressHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace HubCloud.BlazorSheet.EvalEngine.Helpers
+{
+    public class AddressHelper
+    {
+        private static Regex _addressRegex = new Regex(@"R-*\d*C-*\d*", RegexOptions.Compiled);
+        
+        public static string ConvertAddress(string excelAddress)
+        {
+            if (string.IsNullOrEmpty(excelAddress) || _addressRegex.IsMatch(excelAddress))
+                return excelAddress;
+            
+            return excelAddress;
+        }
+    }
+}
---
+++ test/HubCloud.BlazorSheet.UnitTests/AddressHelperTests.cs
@@ -0,0 +1,17 @@
+﻿using HubCloud.BlazorSheet.EvalEngine.Helpers;
+using NUnit.Framework;
+
+namespace HubCloud.BlazorSheet.UnitTests;
+
+[TestFixture]
+public class AddressHelperTests
+{
+    [TestCase("123", "123")]
+    [TestCase("R1C3", "R1C3")]
+    [TestCase("R5C6", "F5")]
+    public void Tests(string inputAddress, string expected)
+    {
+        var outputAddress = AddressHelper.ConvertAddress(inputAddress);
+        Assert.AreEqual(outputAddress, expected);
+    }
+}
