5a578a2b0ec19a9649c4692ac2bb759526eb133b
Vladislav <axid1@mail.ru>
2023-07-12 10:56:56 +0300

    add models and abstractions for converters

---
+++ src/HubCloud.BlazorSheet.EvalEngine/Engine/ExcelFormulaConverter/Abstractions/IExcelToSheetConverter.cs
@@ -0,0 +1,12 @@
+﻿using HubCloud.BlazorSheet.EvalEngine.Engine.ExcelFormulaConverter.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HubCloud.BlazorSheet.EvalEngine.Engine.ExcelFormulaConverter.Abstractions
+{
+    public interface IExcelToSheetConverter
+    {
+        ConvertResult Convert(string excelFormula);
+    }
+}
---
+++ src/HubCloud.BlazorSheet.EvalEngine/Engine/ExcelFormulaConverter/Models/ConvertResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HubCloud.BlazorSheet.EvalEngine.Engine.ExcelFormulaConverter.Models
+{
+    public class ConvertResult
+    {
+        public string Formula { get; set; }
+        public IEnumerable<ConvertException> ConvertExceptions { get; set; }
+    }
+}
