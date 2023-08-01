156124c888a1422dc0eaeaf8372042c8b6b29b0c
Oceanshiver <62504973+Oceanshiver@users.noreply.github.com>
2023-08-01 14:15:45 +0300

    relative addresses

--- test/HubCloud.BlazorSheet.UnitTests/CellShiftFormulaHelperTests.cs
+++ test/HubCloud.BlazorSheet.UnitTests/CellShiftFormulaHelperTests.cs
@@ -33,15 +33,10 @@ public class CellShiftFormulaHelperTests
         var shiftLog = formulaShifter.OnRowAdd(5);
 
         // Assert
-        var formula11 = sheet.GetCell(1, 1).Formula;
-        var formula63 = sheet.GetCell(6, 3).Formula;
-        var formula12 = sheet.GetCell(1, 2).Formula;
-        var formula15 = sheet.GetCell(1, 5).Formula;
-        
-        Assert.AreEqual(formula11, @"VAL(""R7C1"")");
-        Assert.AreEqual(formula63, @"VAL(""R7C1"")");
-        Assert.AreEqual(formula12, @"SUM(""R7C1:R7C1"")+VAL(""R7C1"")");
-        Assert.AreEqual(formula15, @"SUM(""R1C6:R8C6"")");
+        Assert.AreEqual(sheet.GetCell(1, 1).Formula, @"VAL(""R7C1"")");
+        Assert.AreEqual(sheet.GetCell(6, 3).Formula, @"VAL(""R7C1"")");
+        Assert.AreEqual(sheet.GetCell(1, 2).Formula, @"SUM(""R7C1:R7C1"")+VAL(""R7C1"")");
+        Assert.AreEqual(sheet.GetCell(1, 5).Formula, @"SUM(""R1C6:R8C6"")");
     }
     
     [Test]
@@ -66,13 +61,9 @@ public class CellShiftFormulaHelperTests
         var shiftLog = formulaShifter.OnRowAdd(5);
 
         // Assert
-        var formula61 = sheet.GetCell(6, 1).Formula;
-        var formula43 = sheet.GetCell(4, 3).Formula;
-        var formula11 = sheet.GetCell(1, 1).Formula;
-
-        Assert.AreEqual(formula61, @"VAL(""R[-3]C1"")");
-        Assert.AreEqual(formula43, @"VAL(""R[3]C3"")");
-        Assert.AreEqual(formula11, @"SUM(""R1C5:R[6]C5"")");
+        Assert.AreEqual(sheet.GetCell(6, 1).Formula, @"VAL(""R[-3]C1"")");
+        Assert.AreEqual(sheet.GetCell(4, 3).Formula, @"VAL(""R[3]C3"")");
+        Assert.AreEqual(sheet.GetCell(1, 1).Formula, @"SUM(""R1C5:R[6]C5"")");
     }
     
     [Test]
@@ -99,17 +90,41 @@ public class CellShiftFormulaHelperTests
         var shiftLog = formulaShifter.OnColumnAdd(3);
 
         // Assert
-        var formula11 = sheet.GetCell(1, 1).Formula;
-        var formula34 = sheet.GetCell(3, 4).Formula;
-        var formula51 = sheet.GetCell(5, 1).Formula;
-        var formula61 = sheet.GetCell(6, 1).Formula;
-        var formula71 = sheet.GetCell(7, 1).Formula;
+        Assert.AreEqual(sheet.GetCell(1, 1).Formula, @"VAL(""R1C5"")");
+        Assert.AreEqual(sheet.GetCell(3, 4).Formula, @"VAL(""R3C1"")");
+        Assert.AreEqual(sheet.GetCell(5, 1).Formula, @"SUM(""R6C1:R6C5"")");
+        Assert.AreEqual(sheet.GetCell(6, 1).Formula, @"SUM(""R7C5:R7C7"")");
+        Assert.AreEqual(sheet.GetCell(7, 1).Formula, @"SUM(""R7C1:R7C2"")");
+    }
 
-        Assert.AreEqual(formula11, @"VAL(""R1C5"")");
-        Assert.AreEqual(formula34, @"VAL(""R3C1"")");
-        Assert.AreEqual(formula51, @"SUM(""R6C1:R6C5"")");
-        Assert.AreEqual(formula61, @"SUM(""R7C5:R7C7"")");
-        Assert.AreEqual(formula71, @"SUM(""R7C1:R7C2"")");
+    [Test]
+    public void OnColumnAdd_RelativeAddresses_Test()
+    {
+        // Arrange
+        var sheet = GetSheet();
+
+        // value cells
+        sheet.GetCell(1, 4).Value = 100;
+        sheet.GetCell(3, 1).Value = 42;
+        
+        // formula cells
+        sheet.GetCell(1, 1).Formula = @"VAL(""R1C[3]"")";
+        sheet.GetCell(3, 4).Formula = @"VAL(""R3C[-3]"")";
+        sheet.GetCell(5, 1).Formula = @"SUM(""R6C1:R6C[4]"")";
+        sheet.GetCell(7, 1).Formula = @"SUM(""R8C[5]:R8C[7]"")";
+        
+        var workbook = new Workbook();
+        workbook.AddSheet(sheet);
+        
+        // Act
+        IFormulaShifter formulaShifter = new CellShiftFormulaHelper(sheet);
+        var shiftLog = formulaShifter.OnColumnAdd(3);
+
+        // Assert
+        Assert.AreEqual(sheet.GetCell(1, 1).Formula, @"VAL(""R1C[4]"")");
+        Assert.AreEqual(sheet.GetCell(3, 4).Formula, @"VAL(""R3C[-4]"")");
+        Assert.AreEqual(sheet.GetCell(5, 1).Formula, @"SUM(""R6C1:R6C[5]"")");
+        Assert.AreEqual(sheet.GetCell(7, 1).Formula, @"SUM(""R8C[6]:R8C[8]"")");
     }
 
     
