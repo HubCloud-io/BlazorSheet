fdc8dd327501e61e5f7f5e45ccb8c19b8bd4326a
Oceanshiver <62504973+Oceanshiver@users.noreply.github.com>
2023-09-18 17:54:49 +0300

    WorkbookValidator + tests

---
+++ src/HubCloud.BlazorSheet/Validator/CellValidationResult.cs
@@ -0,0 +1,9 @@
+﻿namespace HubCloud.BlazorSheet.Validator;
+
+public class CellValidationResult
+{
+    public Guid SheetUid { get; set; }
+    public string SheetName { get; set; }
+    public string CellAddress { get; set; }
+    public string Message { get; set; }
+}
---
+++ src/HubCloud.BlazorSheet/Validator/WorkbookValidator.cs
@@ -0,0 +1,47 @@
+﻿using HubCloud.BlazorSheet.Core.Models;
+
+namespace HubCloud.BlazorSheet.Validator;
+
+public class WorkbookValidator : IWorkbookValidator
+{
+    public IEnumerable<CellValidationResult> Validate(Sheet sheet)
+    {
+        if (sheet is null)
+            return Enumerable.Empty<CellValidationResult>();
+
+        var resultList = new List<CellValidationResult>();
+
+        var requiredEditCells = sheet.Cells.Where(x => x.EditSettingsUid != null &&
+                                                       sheet.EditSettings.FirstOrDefault(s => s.Uid == x.EditSettingsUid)?.Required == true);
+        
+        foreach (var editCell in requiredEditCells)
+        {
+            var isEmpty = ExpressoFunctions.FunctionLibrary.IsEmptyFunction.Eval(editCell.Value);
+            if (isEmpty)
+                resultList.Add(new CellValidationResult
+                {
+                    SheetUid = sheet.Uid,
+                    SheetName = sheet.Name,
+                    CellAddress = sheet.CellAddress(editCell).ToString()
+                });
+        }
+
+        return resultList;
+    }
+
+    public IEnumerable<CellValidationResult> Validate(Workbook workbook)
+    {
+        if (workbook?.Sheets is null)
+            return Enumerable.Empty<CellValidationResult>();
+        
+        var resultList = new List<CellValidationResult>();
+        foreach (var sheet in workbook.Sheets)
+        {
+            var sheetValidationResults = Validate(sheet)?.ToArray();
+            if (sheetValidationResults?.Any() == true)
+                resultList.AddRange(sheetValidationResults);
+        }
+
+        return resultList;
+    }
+}
