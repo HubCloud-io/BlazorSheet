1b7261a98548023e74c4b33a2e2821a4d567417e
Michael Zyubin <MikhaZyu@gmail.com>
2023-10-04 13:21:16 +0300

    #461-feature: Test page with freezed columns

---
+++ test/HubCloud.BlazorSheet.ExamplesShared/WorkbookBuilders/WorkbookFreezedAreaBuilder.cs
@@ -0,0 +1,22 @@
+﻿using HubCloud.BlazorSheet.Core.Models;
+
+namespace HubCloud.BlazorSheet.ExamplesShared.WorkbookBuilders
+{
+    public class WorkbookFreezedAreaBuilder
+    {
+        public Workbook Build()
+        {
+            var sheetSettings = new SheetSettings();
+            sheetSettings.RowsCount = 10;
+            sheetSettings.ColumnsCount = 30;
+            sheetSettings.FreezedColumns = 4;
+            
+            var sheet = new Sheet(sheetSettings);
+            
+            var workbook = new Workbook();
+            workbook.AddSheet(sheet);
+
+            return workbook;
+        }
+    }
+}
---
+++ test/HubCloud.BlazorSheet.WasmExamples/Client/Pages/SheetFreezedColumnsPage.razor.cs
@@ -0,0 +1,18 @@
+﻿using HubCloud.BlazorSheet.Core.Models;
+using HubCloud.BlazorSheet.ExamplesShared.WorkbookBuilders;
+using Microsoft.AspNetCore.Components;
+
+namespace HubCloud.BlazorSheet.WasmExamples.Client.Pages;
+
+public partial class SheetFreezedColumnsPage: ComponentBase
+{
+    private Sheet _sheet;
+
+    protected override void OnInitialized()
+    {
+        var builder = new WorkbookFreezedAreaBuilder();
+        var workbook = builder.Build();
+
+        _sheet = workbook.FirstSheet;
+    }
+}
