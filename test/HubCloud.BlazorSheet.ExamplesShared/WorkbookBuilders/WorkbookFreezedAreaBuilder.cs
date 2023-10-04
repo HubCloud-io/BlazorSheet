aa788209936b75fd108744bbdfe6d36934940a98
Michael Zyubin <MikhaZyu@gmail.com>
2023-10-04 15:08:24 +0300

    #461-feature: Example with freezed columns

--- test/HubCloud.BlazorSheet.ExamplesShared/WorkbookBuilders/WorkbookFreezedAreaBuilder.cs
+++ test/HubCloud.BlazorSheet.ExamplesShared/WorkbookBuilders/WorkbookFreezedAreaBuilder.cs
@@ -1,4 +1,5 @@
-﻿using HubCloud.BlazorSheet.Core.Models;
+﻿using HubCloud.BlazorSheet.Core.Enums;
+using HubCloud.BlazorSheet.Core.Models;
 
 namespace HubCloud.BlazorSheet.ExamplesShared.WorkbookBuilders
 {
@@ -8,10 +9,35 @@ namespace HubCloud.BlazorSheet.ExamplesShared.WorkbookBuilders
         {
             var sheetSettings = new SheetSettings();
             sheetSettings.RowsCount = 10;
-            sheetSettings.ColumnsCount = 30;
-            sheetSettings.FreezedColumns = 4;
+            sheetSettings.ColumnsCount = 10;
+            //sheetSettings.FreezedColumns = 4;
             
             var sheet = new Sheet(sheetSettings);
+            var column = sheet.GetColumn(1);
+            column.WidthValue = 100;
+
+            column = sheet.GetColumn(2);
+            column.WidthValue = 200;
+
+            var cell = sheet.GetCell(1, 1);
+            cell.Value = "Department";
+
+            var editSettings = new SheetCellEditSettings()
+            {
+                ControlKind = CellControlKinds.Select,
+                CellDataType = 23
+            };
+
+            var editStyle = new SheetCellStyle()
+            {
+                BackgroundColor = "#ebffeb"
+            };
+
+            cell = sheet.GetCell(1, 2);
+            sheet.SetEditSettings(cell, editSettings);
+            sheet.SetStyle(cell, editStyle);
+            
+            sheet.PrepareCellText();
             
             var workbook = new Workbook();
             workbook.AddSheet(sheet);
