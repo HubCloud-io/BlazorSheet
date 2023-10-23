8f723213cf399d9283519979fd4fe44bcd05b862
MAXIM18\18kmv <18kmv@80gmail.com>
2023-10-23 14:21:20 +0300

    #504-bugfix: fixed incorrect string value with integer negative format

--- src/HubCloud.BlazorSheet.Core/Helpers/CellValueFormatHelper.cs
+++ src/HubCloud.BlazorSheet.Core/Helpers/CellValueFormatHelper.cs
@@ -9,11 +9,11 @@ namespace HubCloud.BlazorSheet.Core.Helpers
 {
     public static class CellValueFormatHelper
     {
-        public static string ToStringWithFormat(decimal value, string format)
+        public static string ToStringWithFormat(decimal value, string cellFormat)
         {
             var result = string.Empty;
 
-            switch (format)
+            switch (cellFormat)
             {
                 case CellFormatConsts.Integer:
                     result = value.ToString("##0", CultureInfo.InvariantCulture);
@@ -34,19 +34,22 @@ namespace HubCloud.BlazorSheet.Core.Helpers
                     result = value.ToString("#,##0.000", new CultureInfo(127) { NumberFormat = new NumberFormatInfo { NumberGroupSeparator = " ", NumberDecimalSeparator = "." } });
                     break;
                 case CellFormatConsts.IntegerNegativeWithSpaces:
-                    result = value.ToString("0:#,##0;(#,##0)", new CultureInfo(127) { NumberFormat = new NumberFormatInfo { NumberGroupSeparator = " " } });
+                    var format = value < 0 ? "0:#,##0;(#,##0)" : "#,##0";
+                    result = value.ToString(format, new CultureInfo(127) { NumberFormat = new NumberFormatInfo { NumberGroupSeparator = " " } });
                     break;
                 case CellFormatConsts.IntegerNegativeWithSpacesTwoDecimalPlaces:
-                    result = value.ToString("0:#,##0.00;(#,##0.00)", new CultureInfo(127) { NumberFormat = new NumberFormatInfo { NumberGroupSeparator = " ", NumberDecimalSeparator = "." } });
+                    format = value < 0 ? "0:#,##0.00;(#,##0.00)" : "#,##0.00";
+                    result = value.ToString(format, new CultureInfo(127) { NumberFormat = new NumberFormatInfo { NumberGroupSeparator = " ", NumberDecimalSeparator = "." } });
                     break;
                 case CellFormatConsts.IntegerNegativeWithSpacesThreeDecimalPlaces:
-                    result = value.ToString("0:#,##0.000;(#,##0.000)", new CultureInfo(127) { NumberFormat = new NumberFormatInfo { NumberGroupSeparator = " ", NumberDecimalSeparator = "." } });
+                    format = value < 0 ? "0:#,##0.00;(#,##0.000)" : "#,##0.000";
+                    result = value.ToString(format, new CultureInfo(127) { NumberFormat = new NumberFormatInfo { NumberGroupSeparator = " ", NumberDecimalSeparator = "." } });
                     break;
                 case CellFormatConsts.None:
                     result = value.ToString(new CultureInfo(127) { NumberFormat = new NumberFormatInfo { NumberDecimalSeparator = "." } });
                     break;
                 default:
-                    result = value.ToString(format, CultureInfo.InvariantCulture);
+                    result = value.ToString(cellFormat, CultureInfo.InvariantCulture);
                     break;
             }
 
