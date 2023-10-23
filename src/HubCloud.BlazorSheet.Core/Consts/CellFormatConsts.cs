e91d1fbcd63a13afbfa3a2f2fd23dd3e57874398
MAXIM18\18kmv <18kmv@80gmail.com>
2023-10-23 13:16:46 +0300

    #504-feature: added new number formats, test cases

--- src/HubCloud.BlazorSheet.Core/Consts/CellFormatConsts.cs
+++ src/HubCloud.BlazorSheet.Core/Consts/CellFormatConsts.cs
@@ -7,9 +7,19 @@ namespace HubCloud.BlazorSheet.Core.Consts
     public static class CellFormatConsts
     {
         public const string None = "";
-        public const string Integer = "F0";
-        public const string IntegerTwoDecimalPlaces = "F";
-        public const string IntegerThreeDecimalPlaces = "F3";
+
+        public const string Integer = "100000";
+        public const string IntegerTwoDecimalPlaces = "100000.23";
+        public const string IntegerThreeDecimalPlaces = "100000.234";
+
+        public const string IntegerWithSpaces = "100 000";
+        public const string IntegerWithSpacesTwoDecimalPlaces = "100 000.23";
+        public const string IntegerWithSpacesThreeDecimalPlaces = "100 000.234";
+
+        public const string IntegerNegativeWithSpaces = "(100 000)";
+        public const string IntegerNegativeWithSpacesTwoDecimalPlaces = "(100 000.23)";
+        public const string IntegerNegativeWithSpacesThreeDecimalPlaces = "(100 000.234)";
+
         public const string Date = "dd.MM.yyyy";
         public const string DateTime = "dd.MM.yyyy HH:mm:ss";
     }
